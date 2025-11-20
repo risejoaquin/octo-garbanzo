dc466242166d56375192e0536f395dce602ac7fc
Aaronleon13 <aaronleon13@gmail.com>
2025-11-20 13:47:23 -0700

    feat: Enhance user creation with email validation, implement hard delete, soft delete, and activation routes, and refine user update functionality.

--- src/app.js
+++ src/app.js
@@ -27,17 +27,20 @@ app.get("/users", async (req, res) => {
 });
 
 app.post("/users", async (req, res) => {
-    if (req.body.name || req.body.email || req.body.password) {
+
+    const {name, email, password} = req.body;
+
+    if (!name || !email || !password) {
         return res.status(400).json({error: "El cuerpo de la solicitud debe estar en formato JSON"});
     }
 
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const password_hash = await bcrypt.hash(password, 10);
-
     try {
+        const [emailExists] = await pool.query("SELECT id_user FROM users WHERE email = ?", [email]);
+        if (emailExists.length > 0) {
+            return res.status(400).json({error: "El email ya existe"});
+        }   
+
+        const password_hash = await bcrypt.hash(password, 10);  
         const [result] = await pool.query(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password_hash]
         );
@@ -48,25 +51,56 @@ app.post("/users", async (req, res) => {
     }
 });
 
-app.delete("/users/:id", async (req, res) => {
+app.delete("/users/delete/:id", async (req, res) => {
+    const {id} = req.params;
     try {
-        
+        const [result] = await pool.query("DELETE FROM users WHERE id_user = ?", [id]);
+        res.status(200).json({message: "Usuario eliminado exitosamente", resultado: result});
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({error: "Error al eliminar el usuario"});    
     }
 });
 
-app.put("/users", (req, res) => {
-    res.send("¡Usuario actualizado! PUT").status(200);
+app.put("/users/softdelete/:id", async (req, res) => {
+    const {id} = req.params;
+    try {
+        const [result] = await pool.query("UPDATE users SET is_visible = FALSE WHERE id_user = ?", [id]);
+        res.status(200).json({message: "Usuario eliminado exitosamente", resultado: result});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Error al eliminar el usuario"});    
+    }   
 });
 
-app.patch("/users", (req, res) => {
-    res.send("¡Usuario modificado! PATCH").status(200);
+app.put("/users/active/:id", async (req, res) => {
+    const {id} = req.params;
+    try {
+        const [result] = await pool.query("UPDATE users SET is_visible = TRUE WHERE id_user = ?", [id]);
+        res.status(200).json({message: "Usuario activado exitosamente", resultado: result});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Error al activar el usuario"});    
+    }   
 });
+    
+
+app.put("/users/:id", async (req, res) => {
+    const {id} = req.params;
+    const {name, email} = req.body;
+    try {
+     const result = await pool.query("UPDATE users SET name = ?, email = ? WHERE id_user = ?", [name, email, id]);
+     res.status(200).json({message: "Usuario actualizado exitosamente", resultado: result});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Error al actualizar el usuario"});    
+    }
+});
+
 
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto: ${PORT} 😻`);
+  console.log(`🚀 Servidor corriendo en el puerto: ${PORT}`);
 });
 
  
